a4ba5bfe32fa7362d6f751e56ff677f430b1e64c
NoahPG88 <57874889+NoahPG88@users.noreply.github.com>
2020-03-26 13:38:32 -0500

    exercises part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Have Character';
+  image1 = 'https://akns-images.eonline.com/eol_images/Entire_Site/2019720/rs_600x600-190820151248-600-brad-pitt-inglorious-basterds-me-82019.jpg?fit=around|1200:1200&crop=1200:1200;center,top&output-quality=90';
+  image2 = 'https://images.unsplash.com/photo-1493106819501-66d381c466f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60';
+  image3 = 'https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60';
 
   constructor() { }
 
